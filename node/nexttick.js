0ce18d94d0e17b771724fe7893c66784ba59320e
okamuuu <okamuuu@gmail.com>
2012-08-31 17:50:23 +0900

    modified but it is not my expect.

--- node/nexttick.js
+++ node/nexttick.js
@@ -1,21 +1,75 @@
+var async = require('async');
 
-
-
-getCopied(100000, function(err, result) {
-    console.log(err, result);        
-});
-
-function getCopied(count, callback) {
-
-    if(typeof count !== 'number') {
-        throw new Error('count must be number!!');
-    }
-    
-    for (var i = 0; i < count; i++){
-        
-    }
-     
-    process.nextTick(function() {
-        callback(null, count);
-    });
+function getCopied(delay, callback) {
+    setTimeout(function() {
+        callback(null);
+    }, delay);
 }
+
+function getCopiedWithNextTick(delay, callback) {
+    setTimeout(function() {
+        process.nextTick(function() {
+            callback(null);
+        });
+    }, delay);
+}
+
+// short cut
+function a(count, callback) {
+    getCopied(count, callback);
+}
+
+function b(count, callback) {
+    getCopiedWithNextTick(count, callback);
+}
+
+// !node-bench %
+// run many times so that we can abstract out the overhead of promise creation.
+
+//exports.countPerLap = count;
+
+var len = 1000;
+
+exports.compare = {
+    noNextTick: function(done) {
+
+        var tasks = [
+            function(next) { a(1000, function() {next(null);}); },
+            function(next) { a(1000, function() {next(null);}); },
+            function(next) { a(1000, function() {next(null);}); },
+            function(next) { a(1000, function() {next(null);}); },
+            function(next) { a(1000, function() {next(null);}); },
+            function(next) { a(10,   function() {next(null);}); },
+            function(next) { a(10,   function() {next(null);}); },
+            function(next) { a(10,   function() {next(null);}); },
+            function(next) { a(10,   function() {next(null);}); },
+            function(next) { a(10,   function() {next(null);}); }
+        ];
+        
+        async.parallel(tasks, function() {
+            done();
+        });
+    },
+    withNextTick: function(done) {
+
+        var tasks = [
+            function(next) { b(1000, function() {next(null);}); },
+            function(next) { b(1000, function() {next(null);}); },
+            function(next) { b(1000, function() {next(null);}); },
+            function(next) { b(1000, function() {next(null);}); },
+            function(next) { b(1000, function() {next(null);}); },
+            function(next) { b(10,   function() {next(null);}); },
+            function(next) { b(10,   function() {next(null);}); },
+            function(next) { b(10,   function() {next(null);}); },
+            function(next) { b(10,   function() {next(null);}); },
+            function(next) { b(10,   function() {next(null);}); }
+        ];
+ 
+        async.parallel(tasks, function() {
+            done();
+        });
+    }
+};
+
+
+
